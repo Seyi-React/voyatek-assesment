7cbea89ca4d2a3a605cdb36e4b2405d679eb7015
seyi-react <oluwaseyiadeoti825@gmail.com>
2025-07-17 14:40:59 +0100

    removed theme warning

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 
-const API_BASE_URL = 'https://booking-com15.p.rapidapi.com';
-// const NEXT_BASE_URL = process.env.NEXT_BASE_URL || API_BASE_URL;
+// const API_BASE_URL = 'https://booking-com15.p.rapidapi.com';
+const NEXT_BASE_URL = process.env.NEXT_BASE_URL || "https://booking-com15.p.rapidapi.com/api/v1";
 
 export const apiClient = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: NEXT_BASE_URL,
   headers: {
     'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY || '',
     'X-RapidAPI-Host': 'booking-com15.p.rapidapi.com',
